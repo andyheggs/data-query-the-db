d7b17394c6e694da8332bdc119e435d459b54e94
andy heggs <andy@heggs.net>
2025-01-31 15:37:05 +0000

    SQL_Advanced-Query_the_DB-3rd_Commit

--- queries.py
+++ queries.py
@@ -1,21 +1,22 @@
 # pylint: disable=C0111, C0103
+import sqlite3
 
-def query_orders(db):
+def query_orders(database):
     # Define Query - select all columns from Orders table and sort by OrderID
     query = "SELECT * FROM orders ORDER BY OrderID ASC;"
 
     # Execute query
-    db.execute(query)
+    database.execute(query)
 
     # Fetch Results:
-    results = db.fetchall()
+    results = database.fetchall()
 
     # Return the fetched orders as a list of tuples
     return results
 
-#----------------------------------------------------------------------------------------------------------------------#
 
-def get_orders_range(db, date_from, date_to):
+
+def get_orders_range(database, date_from, date_to):
 
     # SQL Query:
     # - Select all columns from Orders
@@ -26,14 +27,13 @@ def get_orders_range(db, date_from, date_to):
     query = "SELECT * FROM Orders WHERE OrderDate > ? AND OrderDate <= ? ORDER BY OrderDate ASC"
 
     # Execute query
-    results = db.execute(query, (date_from, date_to)).fetchall()
+    results = database.execute(query, (date_from, date_to)).fetchall()
 
     # Return the list of orders within the date range
     return results
 
-#----------------------------------------------------------------------------------------------------------------------#
 
-def get_waiting_time(db):
+def get_waiting_time(database):
 
     # SQL Query:
     # - Select OrderID, OrderDate, ShippedDate
@@ -41,31 +41,31 @@ def get_waiting_time(db):
     # - Use `JULIANDAY()` function to compute date difference in days
     # - Sort results by waiting time (smallest to largest)
 
-    query = "SELECT OrderID, OrderDate, ShippedDate, JULIANDAY(ShippedDate) - JULIANDAY(OrderDate) AS WaitingTime FROM Orders WHERE ShippedDate IS NOT NULL ORDER BY WaitingTime ASC;"
-
-    # Execute the query and fetch all results
-    results = db.execute(query).fetchall()
-
-    # Return the list of orders sorted by waiting time
+    query = """
+    SELECT OrderID, OrderDate, ShippedDate,
+           JULIANDAY(ShippedDate) - JULIANDAY(OrderDate) AS WaitingTime
+    FROM Orders
+    WHERE ShippedDate IS NOT NULL
+    ORDER BY WaitingTime ASC;
+    """
+    results = database.execute(query).fetchall()
     return results
 
-#----------------------------------------------------------------------------------------------------------------------#
 
-import sqlite3
 conn = sqlite3.connect('ecommerce.sqlite')
-db = conn.cursor()
+database = conn.cursor()
 
 # Test query_orders
 print("All Orders Sorted by OrderID:")
-print(query_orders(db))
+print(query_orders(database))
 
 # Test get_orders_range
 print("\nOrders between '2022-01-01' and '2022-03-01':")
-print(get_orders_range(db, '2022-01-01', '2022-03-01'))
+print(get_orders_range(database, '2022-01-01', '2022-03-01'))
 
 # Test get_waiting_time
 print("\nOrders sorted by delivery waiting time:")
-print(get_waiting_time(db))
+print(get_waiting_time(database))
 
 # Close connection
 conn.close()
